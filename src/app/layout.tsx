e6dbbb358a1ca938df4eb8c9410a7f931e2ece28
Alon Goddarde <alongoddard1@gmail.com>
2024-12-16 14:47:11 +0200

    Add better styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "./lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "AI-Chatbot",
@@ -25,10 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={cn(inter.className, "min-h-screen antialiased")}>
+        <main className="h-screen dark text-foreground bg-background">{children}</main>
       </body>
     </html>
-  );
+  )
 }
