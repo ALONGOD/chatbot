0ebafca421fee568d00274acd42ed8e0df116a7e
Alon Goddarde <alongoddard1@gmail.com>
2024-12-16 11:23:25 +0200

    Add redis caching mechanism

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,15 +1,40 @@
-// import { PageProps } from "../../../.next/types/app/layout";
+import { ragChat } from "../lib/rag-chat";
+import { redis } from "../lib/redis";
+
 interface PageProps {
     params: {
         url: string | string[] | undefined;
     }
 }
-const page = (params: PageProps) => {
-    console.log(params);
+
+function reconstructUrl({ url }: { url: string[] }) {
+    const decodedComponents = url.map((component) => decodeURIComponent(component))
+    return decodedComponents.join("/");
+}
+
+
+const page = async ({ params }: PageProps) => {
+    const reconstructedUrl = reconstructUrl({ url: params.url as string[] });
+
+    const isAlreadyIndexed = await redis.sismember("indexed-urls", reconstructedUrl);
+    console.log(isAlreadyIndexed);
+
+
+    if (!isAlreadyIndexed) {
+        await ragChat.context.add({
+            type: "html",
+            source: reconstructedUrl,
+            config: { chunkOverlap: 50, chunkSize: 200 },
+        });
+    }
+
+    await redis.sadd("indexed-urls", reconstructedUrl);
+
     return (
         <div>
             <h1>Page</h1>
         </div>
     );
-}
+};
+
 export default page;
